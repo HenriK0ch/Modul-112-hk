edb5faec426dccf556aff05933e639958465d375
HenriK0ch <hkoch@LAPTOP-S6TIT4JU>
2023-05-22 09:01:34 +0200

    Testing update

---
+++ Testing/SayHelloScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hello, this is my first Script!"
+
+name="Henri"
+num=5
+echo "$name macher"
+echo $num
+datum=$(date +%Y_%m_%d)
+echo $datum
+my_array=(apfel banane)
+echo ${my_array[0]}
